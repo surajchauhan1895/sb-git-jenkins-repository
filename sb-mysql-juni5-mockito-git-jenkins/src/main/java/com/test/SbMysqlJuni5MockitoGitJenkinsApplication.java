37906a4c9bab9783fa128de6a2e71f256f332849
surajchauhan1895 <suraj01895@gmail.com>
2023-01-29 14:14:33 +0530

    spring boot mysql junit5 mockito git repo jenkins project created.

---
+++ sb-mysql-juni5-mockito-git-jenkins/src/main/java/com/test/SbMysqlJuni5MockitoGitJenkinsApplication.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMysqlJuni5MockitoGitJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbMysqlJuni5MockitoGitJenkinsApplication.class, args);
+		
+		System.out.println("Welcome to Spring Boot MySql Junit5 Mockito Git Repo Jenkins CI-CD Pipeline...!!!");
+	}
+
+}
---
+++ sb-mysql-juni5-mockito-git-jenkins/src/test/java/com/test/SbMysqlJuni5MockitoGitJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbMysqlJuni5MockitoGitJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
